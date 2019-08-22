1903315311ae611173268d670fbb877c5be0927b
tiico <ockelberg@gmail.com>
2019-08-22 12:50:04 +0200

    setting up travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. i DID
+          Edit <code>src/App.js</code> and save to reload. I DID
         </p>
         <a
           className="App-link"
